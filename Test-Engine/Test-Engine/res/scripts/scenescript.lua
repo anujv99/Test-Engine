5cd2f800ff6e61c0c4b6c17214055af2bf698d47
PreverseWharf45 <preverse45you@gmail.com>
2018-09-18 16:49:47 +0530

    Testing Verison control

--- Test-Engine/Test-Engine/res/scripts/scenescript.lua
+++ Test-Engine/Test-Engine/res/scripts/scenescript.lua
@@ -1,3 +1,3 @@
 print('SCRIP::Hello Lua!')
-createTerrain(256, 512, 20.0, "hm3.jpg", "res/textures/hm3.jpg")
+--createTerrain(256, 512, 20.0, "hm3.jpg", "res/textures/hm3.jpg")
 setSun(-1, -1, 1, 255, 197, 143)
